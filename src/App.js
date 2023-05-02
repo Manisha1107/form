99ab07c68402cd04d8aad9c89cf205ca866b17e1
Sandhya <neupanesandhya4567@gamil.com>
2023-05-03 00:59:36 +0545

    updated topform

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 // // import logo from './logo.svg';
 // import './App.css';
 import Bottom from "./Bottom"
+import Heading from './Heading';
+import Form from './Form' ;
+import './Form.css';
 
 function App() {
   return (
-   <>
-    <Bottom/>
-   </>
+    <>
+    <div className='register'>
+     <Heading></Heading>
+     <Form></Form>
+     <Bottom/>
+     </div>
+    </>
 
-   
+    
   );
 }
 
 export default App;
+
+
+
