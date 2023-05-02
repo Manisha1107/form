d474c3a61b8e526e2ede931126b250a1922b907c
Manisha <manisapkhrl@gmail.com>
2023-05-02 22:48:35 +0545

    initialcommit

---
+++ src/Bottom.js
@@ -0,0 +1,35 @@
+
+
+function Bottom(){
+    return(
+        <div>
+        <div className='radiobutton'>
+            <label for="gender">gender</label><br/>
+
+            
+            <label for="gender">male</label>
+           
+           
+           <input type="radio" name="gender" id="male"/>
+           <label for="gender">female</label>
+
+           <input type="radio" name="gender" id="female"/>
+
+
+            
+
+        </div>
+        <div className='address'>
+        <label for='address'></label>
+        </div>
+
+
+
+        
+
+        </div>
+    )
+}
+export default Bottom;
+
+
